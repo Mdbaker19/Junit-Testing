7cfbdc5566aaea02f9284a3deaccd466393a63bc
Matthew <matthew.d.baker20@gmail.com>
2021-01-07 10:48:40 -0600

    test exercise one

--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,10 +1,45 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
 public class MyTests {
 
     @Test
     public void stringsEqual(){
         String correct = "Codeup";
+        String input = "Codeup";
+
+        assertEquals(correct, input);
+    }
+
+    @Test
+    public void arraySame(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages, moreLanguages);
+    }
+
+    @Test
+    public void arrayEqual(){
+        int[] nums = {1,2,3};
+        int[] otherNums = new int[3];
+        otherNums[0] = 1;
+        otherNums[1] = 2;
+        otherNums[2] = 3;
+        assertArrayEquals(nums, otherNums);
+    }
+
+    @Test
+    public void checkContains(){
+        String language = "PHP";
+
+        assertTrue(language.contains("H"));
+        assertFalse(language.contains("J"));
 
     }
 
